7a9b06484c7da0d1d9d8866087d3897efd164c07
Javad <javad.zahedi2001@gmail.com>
2021-09-18 11:04:54 +0430

    auth token api was added

--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -18,10 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shortener/', include('apps.shortener.urls')),
     path('public/', include('apps.public.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
