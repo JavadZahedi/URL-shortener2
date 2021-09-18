a4d4282b74bb255c1cdf94f06881cf7497e8b20c
Javad <javad.zahedi2001@gmail.com>
2021-09-18 12:59:18 +0430

    http methods was commented

--- src/backend/apps/public/urls.py
+++ src/backend/apps/public/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'public'
 urlpatterns = [
+    # GET
     path('', views.IndexView.as_view(), name='home'),
 ]
--- src/backend/apps/shortener/urls.py
+++ src/backend/apps/shortener/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'shortener'
 urlpatterns = [
+    # GET, POST
     path('urls/', views.URLView.as_view(), name='urls'),
-    path('url-redirect/<slug:slug>', views.URLRedirectView.as_view(), name='url-redirect')
+    # GET
+    path('url-redirect/<slug:slug>',
+          views.URLRedirectView.as_view(),
+          name='url-redirect'),
 ]
