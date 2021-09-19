5b9a900d1232fe16b4f8ffadee357aad13a0106b
Javad <javad.zahedi2001@gmail.com>
2021-09-19 14:20:12 +0430

    some fields was added to URLSerializer

--- src/apps/shortener/serializers.py
+++ src/apps/shortener/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 from .models import URL
 
 class URLSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = URL
-        fields = ('label', 'address', 'slug', 'visits')
-        read_only_fields = ('visits', 'slug')
+        fields = (
+            'label', 'address', 'slug', 'visits', 'created', 'last_visit'
+        )
+        read_only_fields = ('slug', 'visits', 'created', 'last_visit')
